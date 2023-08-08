55c427b2fb7d101c3ba7388cf063940410e6075a
Rishabh Kumar Gaud <65586827+Rishabh-Gaud@users.noreply.github.com>
2023-08-09 00:11:05 +0530

    v0.1 completed

---
+++ components/Content.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div>
+        <h1 className="text-center">Content</h1>
+        
+    </div>
+  )
+}
+
+export default Content
